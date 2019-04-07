fa91d3beebd9ab3578e8b62a9b654abf85d4c58b
israel <israel.bejarano.a@gmail.com>
2019-04-07 19:32:44 +0200

    maquetar encuesta componente

--- src/app/components/encuesta/encuesta.component.ts
+++ src/app/components/encuesta/encuesta.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EncuestaComponent implements OnInit {
 
+  public barChartOptions: any = {
+    responsive: true,
+    scaleShowVerticalLines: false
+  };
+
+  public barChartLabels: string[] = ['Pregunta 1', 'Pregunta 2', 'Pregunta 3', 'Pregunta 4'];
+  public barChartType = 'bar';
+
+  public barChartData: any[] = [
+    {data: [ 65, 59, 80, 81 ], label: 'Entrevistados'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
