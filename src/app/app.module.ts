3a9ba2b0ebeb9d579ccd404837ddeaa12abeecc2
israel <israel.bejarano.a@gmail.com>
2019-04-07 13:38:36 +0200

    declarcion de modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
+const config: SocketIoConfig = { url: 'http://localhost:5000', options: {} };
+
+import { ChartsModule } from 'ng2-charts';
 import { AppComponent } from './app.component';
+import { EncuestaComponent } from './components/encuesta/encuesta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncuestaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config),
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
